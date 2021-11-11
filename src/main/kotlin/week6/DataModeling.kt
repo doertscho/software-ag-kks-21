58d512bcd77b8f8b54601fcad246da404a286be8
Stefan Röpstorff <sroepstorff@natureconnect.eco>
2021-11-11 18:05:38 +0100

    add comments in code for week 6

--- src/main/kotlin/week6/DataModeling.kt
+++ src/main/kotlin/week6/DataModeling.kt
@@ -32,29 +32,43 @@ data class Move(
     val target: Position,
 )
 
+// Selbst definierte Datentypen können ebenso Eingaben für Funktionen sein:
+fun describeChessFigure(figure: ChessFigure) {
+    println("Die Farbe der Figur ist: ${figure.colour}")
+    println("Der Typ der Figur ist: ${figure.type}")
+    println("Die Figur heißt: ${figure.name}")
+}
+
 fun main() {
 
     val blackKing = ChessFigure(Type.King, Colour.Black)
 
+    // Der folgende Code erstellt uns ein zweidimensionales Array mit 8x8 Feldern,
+    // die alle anfangs leer sind, also den null-Wert enthalten.
     val chessBoard: Array<Array<ChessFigure?>> = Array(8) {
         Array(8) {
             null
         }
     }
 
+    // Die einzelnen Felder können wir nun gezielt mit "echten" Werten füllen:
     chessBoard[6][2] = ChessFigure(Type.Pawn, Colour.White)
     chessBoard[1][2] = ChessFigure(Type.Knight, Colour.White)
     chessBoard[2][5] = blackKing
 
+    // So kann ich auf Bestandteile meines komplexen Datentyps zugreifen:
     val chessFigure = ChessFigure(Type.Queen, Colour.Black)
     println("Die Farbe der Figur ist: ${chessFigure.colour}")
     println("Der Typ der Figur ist: ${chessFigure.type}")
+    // Ich kann diesen komplexen Wert auch als Ganzes an meine Funktion übergeben:
+    describeChessFigure(chessFigure)
 
+    // Wenn ein komplexer Wert wiederum komplexe Werte als Bestandteil enthält,
+    // kann ich mehrere Zugriffe aneinanderreihen:
     val firstMove = Move(
         ChessFigure(Type.Pawn, Colour.White),
         Position(1, 5),
         Position(3, 5),
     )
-
-    firstMove.target.row
+    val targetRowOfFirstMove = firstMove.target.row
 }
